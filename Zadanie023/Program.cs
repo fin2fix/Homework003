5b60cbf2a454b669d63c58d44b8a846b06e63424
KMikhail <fin2fix@gmail.com>
2023-01-24 12:34:14 +0000

    Add Zadanie023

---
+++ Zadanie023/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+Задача 23
+
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+int GetNumber(string message)
+{
+  int resultNumber = 0;
+
+  while (true)
+  {
+    Console.WriteLine(message);
+
+    if (int.TryParse(Console.ReadLine(), out resultNumber) && resultNumber > 1)
+    {
+      break;
+    }
+    else
+    {
+      Console.WriteLine("Ввели не число или не корректное число. Повторите ввод!");
+      Console.WriteLine("-----------");
+    }
+  }
+  return resultNumber;
+}
+
+void Cubelist(int digit)
+{
+  int cubeNumber = 0;
+  for (int i = 1; i < digit; i++)
+  {
+    cubeNumber = i * i * i;
+    Console.Write($"{cubeNumber}, ");
+  }
+  Console.Write($"{digit * digit * digit}.");
+}
+
+int number = GetNumber("Введите число больше 1");
+Cubelist(number);
+
